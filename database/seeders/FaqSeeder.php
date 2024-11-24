f65202c4bd48ed76189e78d9574f85f54bd9977a
James Ilemona <ilemonaj@gmail.com>
2024-11-25 00:25:36 +0100

    updatre

--- database/seeders/FaqSeeder.php
+++ database/seeders/FaqSeeder.php
@@ -26,12 +26,12 @@ class FaqSeeder extends Seeder
             'question' => 'How can I schedule an appointment?',
             'answer' => 'You can easily schedule an appointment by contacting our office via phone or email, or by filling out our online appointment request form.',
          ]);
-        Faq::Create([  'question' => 'Are your services covered by insurance?',
-            'answer' => 'We accept a variety of insurance plans, and our team can help you navigate the process to ensure you receive the coverage you are entitled to.',
-        ]);
+        // Faq::Create([  'question' => 'Are your services covered by insurance?',
+        //     'answer' => 'We accept a variety of insurance plans, and our team can help you navigate the process to ensure you receive the coverage you are entitled to.',
+        // ]);
         Faq::Create([
             'question' => 'Do you offer specialized therapy for specific issues?',
-            'answer' => 'Our psychologists have expertise in various areas, such as anxiety, depression, trauma, and more, offering specialized therapy tailored to your unique needs.',
+            'answer' => 'Our Therapy team have expertise in various areas, such as anxiety, depression, trauma, and more, offering specialized therapy tailored to your unique needs.',
         ]);
         Faq::Create([
             'question' => 'Can I choose my therapist?',
