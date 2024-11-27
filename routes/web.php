05ef710c9e8ec737e759c3e4c01e22cf3c2052d5
James Ilemona <ilemonaj@gmail.com>
2024-11-27 18:33:49 +0100

    update

--- routes/web.php
+++ routes/web.php
@@ -332,8 +332,8 @@ Route::middleware(['auth:sanctum','verified',])->group(function () {
 // Route::get('/video-call', function () {
 //     return view('video-call');
 // })->name('video-call');
-Route::get('/term-condition', function () {     return view('term-condition');
- })->name('term-condition');
+Route::get('/terms', function () {     return view('information.terms');
+ })->name('terms');
 // Route::get('/social-media', function () {
 //     return view('social-media');
 // })->name('social-media');
